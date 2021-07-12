f2dcb9fcccd2aebf29e0207873cae6c60aa039be
Nischit Shetty <njmshetty@gmail.com>
2021-07-12 19:13:35 +0530

    Update CheckoutProduct.js

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -3,7 +3,7 @@ import './CheckoutProduct.css'
 import { useStateValue } from './StateProvider';
 
 function CheckoutProduct({id,title, price, imageUrl}) {
-    const [ dispatch] = useStateValue();
+    const [{basket}, dispatch] = useStateValue();
 
     const removeFromBasket =() => {
         dispatch({
